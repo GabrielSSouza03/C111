f70e884272d44327ac3b194fc6d504a73c40165f
Gabriel <127989089+GabrielSSouza03@users.noreply.github.com>
2025-05-16 21:24:32 -0300

    Add files via upload

---
+++ Cap.6/cap06.py
@@ -0,0 +1,10 @@
+import numpy as np 
+import pandas as pd 
+import matplotlib.pyplot as plt
+
+#PLOT 
+# Descreve as alterações nos dados ao longo do tempo 
+x = np.array([1,2,3,4])
+y = x*2
+
+plt.plot(x,y)
